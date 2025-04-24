74d3a3809b1e54b58bebb86224b7a5508e65a1ec
ltian@beyond.ai <627096931@qq.com>
2025-04-24 19:29:56 +0800

    feature: init commit

---
+++ src/main/java/com/test/TestServer/enums/Species.java
@@ -0,0 +1,14 @@
+package com.test.TestServer.enums;
+
+import com.test.TestServer.entity.Pet;
+
+/**
+ * The species of a pet. Primarily used on the {@link Pet} entity.
+ */
+public enum Species {
+    dog,
+    cat,
+    rabbit,
+    hamster,
+    bird
+}
